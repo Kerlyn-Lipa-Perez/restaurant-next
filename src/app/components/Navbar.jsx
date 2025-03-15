e3a31cc46ae9502f23597d7dfe0d0725a934a7f4
Kerlyn Lipa <kerlynlipaperez@gmail.com>
2025-03-15 02:22:51 -0500

    editing files

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Link from 'next/link'
 function Navbar() {
   return (
-    <div>Navbar</div>
+   <>
+   <Link href="/">Home</Link>
+   <Link href="/about">About</Link>
+   <Link href="/contact">Contact</Link>
+   </>
   )
 }
 
---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+
+export default function NotFound() {
+  return (
+    <section>
+ 
+        <div>No encontrado</div>
+        <Link href="/">Home</Link>
+    </section>
+  )
+}
